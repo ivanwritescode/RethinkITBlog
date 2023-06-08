b8f9158ff7dafd26cbd5cc9c3e7ed9508b319e38
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-08 15:15:40 +0800

    Added a functionality that associates a blog post to a speficic author.

--- blogposts/createpost.php
+++ blogposts/createpost.php
@@ -1,26 +1,37 @@
 <?php
+session_start(); // Start the session
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = $_POST['title'];
     $content = $_POST['content'];
     $date = date('Y-m-d H:i:s'); // Get the current date and time
 
-    // TODO: Perform any necessary data validation and sanitization
-    // TODO:  Associate an authorid later for each blog post
+    // Check if the user ID session variable is set
+    if (isset($_SESSION['id'])) {
+        $userId = $_SESSION['id']; // Get the user ID from the session
 
-    include '../utils/db_connect.php';
+        include '../utils/db_connect.php';
 
-    $sql = "INSERT INTO blogposts (title, content, date) VALUES ('$title', '$content', '$date')";
+        $sql = "INSERT INTO blogposts (title, content, date, authorid) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sssi", $title, $content, $date, $userId);
 
-    if ($conn->query($sql) === TRUE) {
-        // Display if the post creation was successful
-        echo "Post created successfully!";
-        header("Location: ../home.php");
-        exit(); // just to make sure to exit the script after redirection
+        if ($stmt->execute()) {
+            // Display if the post creation was successful
+            echo "Post created successfully!";
+            header("Location: ../home.php");
+            exit(); // just to make sure to exit the script after redirection
+        } else {
+            // Display if an error occurred while creating the post
+            echo "Error: " . $stmt->error;
+        }
+
+        $stmt->close();
+        $conn->close();
     } else {
-        // Display if an error occurred while creating the post
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        // User is not logged in, handle the case accordingly
+        echo "User not logged in.";
     }
-
-    $conn->close();
 }
 ?>
+
