8063efdc67feafd9d8d2bce1e266739a8eaf0a23
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-08 12:40:26 +0800

    Edit createpost.php action to add current date

--- blogposts/createpost.php
+++ blogposts/createpost.php
@@ -2,13 +2,14 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = $_POST['title'];
     $content = $_POST['content'];
+    $date = date('Y-m-d H:i:s'); // Get the current date and time
 
     // TODO: Perform any necessary data validation and sanitization
     // TODO:  Associate an authorid later for each blog post
 
     include '../utils/db_connect.php';
 
-    $sql = "INSERT INTO blogposts (title, content) VALUES ('$title', '$content')";
+    $sql = "INSERT INTO blogposts (title, content, date) VALUES ('$title', '$content', '$date')";
 
     if ($conn->query($sql) === TRUE) {
         // Display if the post creation was successful
