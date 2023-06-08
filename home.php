4893a8c8411faeace239f5663572ac639f35d455
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-08 13:43:28 +0800

    Nope. Moment.js back on top for now util I resolve the reference issue.

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
 </head>
 <body>
     <div class="container">
@@ -55,7 +56,6 @@
     </div>
 </body>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
 
 <script>
     function deletePost(postId) {
