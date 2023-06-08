969abe7e2dd267b787575dbb173145826bd11bbb
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-08 19:39:37 +0800

    Updated Create a Post page card layout

--- blogposts/createpostform.php
+++ blogposts/createpostform.php
@@ -12,21 +12,25 @@
     <?php include '../header.php'; ?>
     <div class="container">
         <main>
-            <h2 class="section-title">Create a Post!</h1>
             <div class="card card-bg-inherit">
-                <form action="createpost.php" method="POST">
-                    <p class="form-group">
-                        <label for="title">Enter Title</label>
-                        <input type="text" id="title" name="title" required>
-                    </p>
-                    <br>
-                    <p class="form-group">
-                        <label for="content">Enter Content</label>
-                        <textarea id="content" name="content" required></textarea>
-                    </p>
-                    <br>
-                    <button class="btn btn-submit" type="submit">POST</button>
-                </form>
+                <div class="card-header">
+                    <h2>Create a Post!</h1>
+                </div> 
+                <div class="card-body">
+                    <form action="createpost.php" method="POST">
+                        <p class="form-group">
+                            <label for="title">Enter Title</label>
+                            <input type="text" id="title" name="title" required>
+                        </p>
+                        <br>
+                        <p class="form-group">
+                            <label for="content">Enter Content</label>
+                            <textarea id="content" name="content" required></textarea>
+                        </p>
+                        <br>
+                        <button class="btn btn-submit" type="submit">POST</button>
+                    </form>
+                </div>
             </div>
         </main>
     </div>
