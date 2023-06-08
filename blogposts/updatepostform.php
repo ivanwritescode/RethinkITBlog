d684a277339480fd295934fd8733698e038fa019
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-08 16:39:47 +0800

    Added Edit Post page styles

--- blogposts/updatepostform.php
+++ blogposts/updatepostform.php
@@ -10,38 +10,40 @@
 </head>
 <body>
     <?php include '../header.php'; ?>
-    <main>
-        <?php
-        $blogpostId = $_GET['blogpost_id'];
-
-        include '../utils/db_connect.php';
-
-        $sql = "SELECT * FROM blogposts WHERE id = $blogpostId";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            $post = $result->fetch_assoc();
-        ?>
-        <h1>Edit Post - <?php echo $post['title']; ?></h1>
-        <div class="card">
-            <form action="updatepost.php" method="POST">
-                <label for="title">Enter New Title:</label>
-                <input type="text" id="title" name="title" value="<?php echo $post['title']; ?>" required>
-                <br>
-                <label for="content">Enter New Content:</label>
-                <textarea id="content" name="content" required><?php echo $post['content']; ?></textarea>
-                <br>
-                <input type="hidden" name="post_id" value="<?php echo $post['id']; ?>">
-                <button type="submit">SAVE</button>
-            </form>
+    <div class="container">
+        <main>
             <?php
-            } else {
-                echo "Post not found.";
-            }
+            $blogpostId = $_GET['blogpost_id'];
 
-            $conn->close();
+            include '../utils/db_connect.php';
+
+            $sql = "SELECT * FROM blogposts WHERE id = $blogpostId";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                $post = $result->fetch_assoc();
             ?>
-        </div>
-    </main>
+            <h2 class="section-title">Edit Post - <?php echo $post['title']; ?></h1>
+            <div class="card">
+                <form action="updatepost.php" method="POST">
+                    <label for="title">Enter New Title:</label>
+                    <input type="text" id="title" name="title" value="<?php echo $post['title']; ?>" required>
+                    <br>
+                    <label for="content">Enter New Content:</label>
+                    <textarea id="content" name="content" required><?php echo $post['content']; ?></textarea>
+                    <br>
+                    <input type="hidden" name="post_id" value="<?php echo $post['id']; ?>">
+                    <button type="submit">SAVE</button>
+                </form>
+                <?php
+                } else {
+                    echo "Post not found.";
+                }
+
+                $conn->close();
+                ?>
+            </div>
+        </main>
+    </div>
 </body>
 </html>
