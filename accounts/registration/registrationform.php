3e7506c7d2a23e87f0cd49eff0f682659387e51e
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-08 20:22:58 +0800

    Fixed document title

--- accounts/registration/registrationform.php
+++ accounts/registration/registrationform.php
@@ -10,7 +10,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>MiniBlog</title>
+	<title>MiniBlog - Register</title>
 	<link rel="stylesheet" href="../../public/styles/reset.css">
 	<link rel="stylesheet" href="../../public/styles/styles.css">
 </head>
